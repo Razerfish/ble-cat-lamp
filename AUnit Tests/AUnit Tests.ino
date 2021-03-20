a36f4e4727832d74689184c58d18ecb421fc0194
Fiona Wilson <fiona@razerfish.dev>
2021-03-20 16:05:13 -0400

    Add boilerplate test code

--- AUnit Tests/AUnit Tests.ino
+++ AUnit Tests/AUnit Tests.ino
@@ -4,12 +4,19 @@
  Author:	fiona.wilson
 */
 
+#include <AUnitVerbose.h>
+
+// Include files to test here.
+
+
+// Define tests here.
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	Serial.begin(115200);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	aunit::TestRunner::run();
 }
