917190ca1d4ec40556b0a9cbb1ea03abeea64fe7
Fiona Wilson <fiona@razerfish.dev>
2021-03-29 13:53:57 -0400

    Update name in Main.ino

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,15 +1,13 @@
 /*
  Name:		Main.ino
  Created:	3/29/2021 1:45:57 PM
- Author:	Fiona College
+ Author:	Fiona Wilson
 */
 
-// the setup function runs once when you press reset or power the board
 void setup() {
 
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
   
 }
